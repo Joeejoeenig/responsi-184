2e55f294a30d651aec1bc95b347be514c83084de
Joeejoeenig <joeejoeenig@gmail.com>
2026-07-18 08:45:45 +0700

    commit
    commit update views

---
+++ HarryMaguire/lib/views/spell/spell_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SpellPage extends StatelessWidget {
+  const SpellPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Spell Page"),
+      ),
+    );
+  }
+}
